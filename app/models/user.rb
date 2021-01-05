4acc99e67511af6a1df8f7e8e31b82be2ceac1cc
Christian Lampl <christian.lampl@gmx.at>
2021-01-04 21:51:54 -0300

    Add: 'following' feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,28 @@ class User < ApplicationRecord
 
   has_many :tweets
   has_many :likes
+  has_many :following, class_name: 'Following', foreign_key: 'user_id'
+  has_many :followed_by, class_name: 'Following', foreign_key: 'following_user'
 
   def to_s
     name
+  end
+
+  def follow(other_user)
+    Following.create(user: self, following_user: other_user)
+  end
+
+  def unfollow(other_user)
+    following.where(following_user: other_user).destroy_all
+  end
+
+  def followed_by_users
+    followed_by.map(&:user)
+  end
+
+  alias followers followed_by_users
+
+  def following_users
+    following.map(&:following_user)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_022020) do
+ActiveRecord::Schema.define(version: 2021_01_04_235748) do
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "following_user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["following_user_id"], name: "index_followings_on_following_user_id"
+    t.index ["user_id"], name: "index_followings_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -45,6 +54,8 @@ ActiveRecord::Schema.define(version: 2020_12_13_022020) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "followings", "users"
+  add_foreign_key "followings", "users", column: "following_user_id"
   add_foreign_key "likes", "tweets"
   add_foreign_key "likes", "users"
   add_foreign_key "tweets", "tweets"
