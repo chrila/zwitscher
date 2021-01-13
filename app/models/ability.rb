e460ba2c984e26380702af2467052b1e25d10f07
Christian Lampl <christian.lampl@gmx.at>
2021-01-13 18:59:56 -0300

    Add: do not allow a user to unfollow himself

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.present? && user.user_type == 'personal'
-      can :destroy, Tweet, user_id: user.id
+    if user.present?
+      can :follow, User
+      cannot :follow, User, id: user.id
+      
+      if user.user_type == 'personal'
+        can :destroy, Tweet, user_id: user.id
+      end
     end
   end
 end
