c56ef868638b97114b8ff6966fc487c9fac97757
Christian Lampl <christian.lampl@gmx.at>
2021-04-11 13:09:34 -0400

    Add: improved handling of follow/unfollow via ujs

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,12 @@
+
 const { environment } = require('@rails/webpacker')
-const jquery = require('./plugins/jquery')
+
+module.exports = environment
 
 const webpack = require('webpack')
-environment.plugins.append('Provide',
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
   })
 )
-
-environment.plugins.prepend('jquery', jquery)
-module.exports = environment
